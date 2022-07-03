fdba8fdc3fceaabe20d13dd719430e9f92ce3646
Dominik Filipovic <filjo2409@gmail.com>
2022-07-03 18:36:10 +0200

    implemented services

---
+++ BlazorFullStackCrud/Client/Services/SuperHeroService/SuperHeroService.cs
@@ -0,0 +1,48 @@
+﻿using System.Net.Http.Json;
+
+namespace BlazorFullStackCrud.Client.Services.SuperHeroService
+{
+    public class SuperHeroService : ISuperHeroService
+    {
+        private readonly HttpClient httpClient;
+
+        public SuperHeroService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+        public List<SuperHero> SuperHeroes { get; set; } = new List<SuperHero>();
+        public List<Comic> Comics { get; set; } = new List<Comic>();
+
+        public async Task GetComics()
+        {
+            var result = await httpClient.GetFromJsonAsync<List<Comic>>("api/superhero/comics");
+
+            if (result != null)
+            {
+                Comics = result;
+            }
+            
+        }
+
+        public async Task<SuperHero> GetSingleHero(int id)
+        {
+            var result = await httpClient.GetFromJsonAsync<SuperHero>($"api/superhero/{id}");
+
+            if (result != null)
+            {
+                return result;
+            }
+            return null;
+        }
+
+        public async Task GetSuperHeroes()
+        {
+            var result = await httpClient.GetFromJsonAsync<List<SuperHero>>("api/superhero");
+
+            if (result != null)
+            {
+                SuperHeroes = result;
+            }
+        }
+    }
+}
---
+++ BlazorFullStackCrud/Server/Controllers/SuperHeroController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorFullStackCrud.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        public static List<Comic> comics = new List<Comic> {
+
+            new Comic{ Id=1, Name="Marvel"},
+            new Comic{Id=2, Name="DC"}
+        };
+
+        public static List<SuperHero> superHeroes = new List<SuperHero>{
+
+            new SuperHero { Id = 1, FirstName = "Peter", LastName = "Parker", HeroName = "Spiderman", Comic=comics[0], ComicId = 1 },
+            new SuperHero { Id = 2, FirstName = "Bruce", LastName = "Wayne", HeroName = "Batman", Comic = comics[1], ComicId = 2 },
+
+        };
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetSuperHeroeos()
+        {
+
+            if (superHeroes == null)
+            {
+                return NotFound();
+            }
+            try
+            {
+                return Ok(superHeroes);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            
+        }
+        [HttpGet("comics")]
+        public async Task<ActionResult<List<Comic>>> GetComics()
+        {
+
+            if (comics == null)
+            {
+                return NotFound();
+            }
+            try
+            {
+                return Ok(comics);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SuperHero>> GetSingleHero(int id)
+        {
+            var hero = superHeroes.FirstOrDefault(x => x.Id == id);
+
+            if (superHeroes == null)
+            {
+                return NotFound();
+            }
+            try
+            {
+                return Ok(hero);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+    }
+}
